d817c005f6f4c0be09d29fbb067df10fbbf46314
Matthias Ernst <matthias.ernst@students.fhnw.ch>
2017-10-12 23:09:02 +0900

    WIP-word counter
    also added a way to load text

--- src/spacy_test.py
+++ src/spacy_test.py
@@ -1,13 +1,25 @@
 import spacy
+
 en_nlp = spacy.load('en')
 de_nlp = spacy.load('de')
-en_doc = en_nlp(u'Hello, world. Here are two sentences.')
-de_doc = de_nlp(u'ich bin ein Berliner.')
-print(spacy)
-print(de_doc)
-print(en_doc)
-
 
 import pandas as pd
+
 df = pd.read_csv('../data/swg2g.CSV', delimiter=';')
 print(df['INFO'][0])
+
+unique_sentences_id = set()
+for id in df['TASK_ID']:
+    unique_sentences_id.add(id)
+
+allTextByTaskID = {}
+for id in unique_sentences_id:
+    allTextByTaskID[id]=[]
+
+for i in range(len(df)):
+    allTextByTaskID[df['TASK_ID'][i]].append(df['INFO'][i])
+
+print(allTextByTaskID) #mit allTextByTaskID[TASK_ID] bekommt man ein array mit allen INFO Saetzen
+
+for task in allTextByTaskID:
+    allTextByTaskID[task] = [x for x in allTextByTaskID[task] if len(x.split()) >= ]
