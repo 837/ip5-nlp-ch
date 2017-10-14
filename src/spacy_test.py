42430b15d5d1bdc085867799a2a5a31423f48ab6
Matthias Ernst <matthias.ernst@students.fhnw.ch>
2017-10-15 00:32:37 +0900

    Added Test File with less swg2g

--- src/spacy_test.py
+++ src/spacy_test.py
@@ -5,7 +5,7 @@ de_nlp = spacy.load('de')
 
 import pandas as pd
 
-df = pd.read_csv('../data/swg2g.CSV', delimiter=';')
+df = pd.read_csv('../data/swg2g_test.CSV', delimiter=';')
 print(df['INFO'][0])
 
 unique_sentences_id = set()
@@ -22,4 +22,6 @@ for i in range(len(df)):
 print(allTextByTaskID) #mit allTextByTaskID[TASK_ID] bekommt man ein array mit allen INFO Saetzen
 
 for task in allTextByTaskID:
-    allTextByTaskID[task] = [x for x in allTextByTaskID[task] if len(x.split()) >= ]
+    allTextByTaskID[task] = [x for x in allTextByTaskID[task] if len(x.split()) >= 3 ]
+
+print(allTextByTaskID)
