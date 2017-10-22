92cf78b73f59e10b347d8b11e5718f216f4571be
Matthias Ernst <matthias.ernst@students.fhnw.ch>
2017-10-22 19:00:23 +0900

    Removed faulty Idea Files

--- src/spacy_test.py
+++ src/spacy_test.py
@@ -5,7 +5,7 @@ de_nlp = spacy.load('de')
 
 import pandas as pd
 
-df = pd.read_csv('../data/swg2g_test.CSV', delimiter=';')
+df = pd.read_csv('../data/transcribe-2017-07-08.CSV', delimiter=';')
 print(df['INFO'][0])
 
 unique_sentences_id = set()
@@ -14,14 +14,21 @@ for id in df['TASK_ID']:
 
 allTextByTaskID = {}
 for id in unique_sentences_id:
-    allTextByTaskID[id]=[]
+    allTextByTaskID[id] = []
 
 for i in range(len(df)):
     allTextByTaskID[df['TASK_ID'][i]].append(df['INFO'][i])
 
-print(allTextByTaskID) #mit allTextByTaskID[TASK_ID] bekommt man ein array mit allen INFO Saetzen
+print(allTextByTaskID)  # mit allTextByTaskID[TASK_ID] bekommt man ein array mit allen INFO Saetzen
 
-for task in allTextByTaskID:
-    allTextByTaskID[task] = [x for x in allTextByTaskID[task] if len(x.split()) >= 3 ]
+# for task in allTextByTaskID:
+#     allTextByTaskID[task] = [x for x in allTextByTaskID[task] if len(x.split()) >= 3 ]
+#
+# print(allTextByTaskID)
 
-print(allTextByTaskID)
+import nltk
+
+hypothesis = allTextByTaskID[2530][0]
+reference = allTextByTaskID[2530][1]
+BLEUscore = nltk.translate.bleu_score.sentence_bleu([reference], hypothesis, weights=[1])
+print(BLEUscore)
