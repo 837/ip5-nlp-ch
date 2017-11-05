922976b2d527a1d0672a23d1f71973aad02c2727
Fabio Strappazzon <fabio.strappazzon@students.fhnw.ch>
2017-11-05 13:06:10 +0100

    added histogram

--- src/spacy_test.py
+++ src/spacy_test.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import nltk
 import util
@@ -10,19 +11,21 @@ def filterTasksLesserOrEqualThan(n, tasks):
 
 
 def bleu_ratings(texts):
+    texts = list(map(lambda x: x.lower(), texts))
     scores = []
     for t in texts:
-        result = nltk.translate.bleu_score.sentence_bleu(list(filter(lambda x: x != t, texts)), t)
+        result = nltk.translate.bleu(list(filter(lambda x: x != t, texts)), t)
         scores.append(result)
     return scores
 
 
 def reject_outliers(data, values, m=2.):
     values = np.array(values)
-    d = np.abs(values - np.median(values))
-    mdev = np.median(d)
-    s = d / (mdev if mdev else 1.)
-    return np.array(data)[s < m].tolist(), values[s < m]
+    # d = np.abs(values - np.median(values))
+    # mdev = np.median(d)
+    # s = d/(mdev if mdev else 1.)
+    # return np.array(data)[s < m].tolist(), values[s < m].tolist()
+    return np.array(data)[values < 0.3].tolist(), values[values < 0.3].tolist()
 
 
 def getGoodTransscriptions(texts):
@@ -37,3 +40,20 @@ print(texts, ratings)
 # for taskID in filterTasksLesserOrEqualThan(2, allTaskByID):
 #    texts, ratings = getGoodTransscriptions(allTaskByID[taskID][0])
 #    print(ratings)
+
+#print(ratings)
+#print(bleu_ratings(allTaskByID[2096][0]))
+
+all = []
+for taskID in filterTasksLesserOrEqualThan(2, allTaskByID):
+    # texts, ratings = getGoodTransscriptions(allTaskByID[taskID][0])
+    ratings = bleu_ratings(allTaskByID[taskID][0])
+    # if len(texts) > 0:
+    #     print(taskID)
+    #     print(texts)
+    for r in ratings:
+        all.append(r)
+
+plt.hist(all, 200)
+plt.show()
+#print(np.sort(mins))
