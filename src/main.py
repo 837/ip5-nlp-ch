f9f873460e115614dff83e9add68a16efaf49e69
Matthias Ernst <matthias.ernst@students.fhnw.ch>
2018-01-03 14:32:27 +0900

    Added missing/additional word counter [WIP]
    does not really work yet

--- src/main.py
+++ src/main.py
@@ -23,63 +23,90 @@ def calculate_alignment_score(gs_graph, alignment_graph, additional_Text="", sho
         map((lambda group: list(group)), nx.connected_components(alignment_graph)))
 
     goldstandardWordCount = 0
-    missingWords = 0
+    words_in_gs_not_in_alignment_counter = 0
     foundWords = 0
     numberOfAlignedGroupes = len(createdAlignmentList)
     numberOfGSGroupes = len(goldstandardList)
     alignedWordCount = 0
+
     for alignments in createdAlignmentList:
         alignedWordCount += len(alignments)
 
+    # additional_word_counter_dict = {}
+    words_in_alignment_not_in_gs_counter_dict = {}
     for alignments in goldstandardList:
         goldstandardWordCount += len(alignments)
+        words_in_alignment_not_in_gs_counter_dict[str(alignments)] = []
         for word in alignments:
             if alignment_graph.has_node(word):
                 foundWords += 1
+                found = nx.node_connected_component(alignment_graph, word)
+                # additional_word_counter_dict[word] = sum(word not in alignments for word in found)
+                # if additional_word_counter_dict[word] > 0:
+                #     print(additional_word_counter_dict[word], word, alignments, found,
+                #           list(word not in alignments for word in found))
+                words_in_alignment_not_in_gs_counter_dict[str(alignments)].append(
+                    sum(word not in alignments for word in found))
             else:
-                print("missing " + word)
-                missingWords += 1
+                print("missing: " + word)
+                words_in_gs_not_in_alignment_counter += 1
 
-    scoreWords = (foundWords / goldstandardWordCount) * 100
-    scoreGroups = (abs(numberOfGSGroupes - numberOfAlignedGroupes) / (
+##########################PLEASE REDO :D ###################################
+    # print(words_in_alignment_not_in_gs_counter_dict)
+    words_in_alignment_not_in_gs_counter = sum(
+        arr[0] for arr in words_in_alignment_not_in_gs_counter_dict.values() if len(arr) > 0)
+    word_count_score = (foundWords / goldstandardWordCount) * 100
+
+    score_groups = (abs(numberOfGSGroupes - numberOfAlignedGroupes) / (
         numberOfGSGroupes + numberOfAlignedGroupes)) * 100
-    totalScore = scoreWords - scoreGroups
+    words_not_in_gs_percentage = (words_in_alignment_not_in_gs_counter / alignedWordCount) * 100
+    words_not_in_alignment_percentage = (words_in_gs_not_in_alignment_counter / goldstandardWordCount) * 100
+
+    score_words = (word_count_score - words_not_in_gs_percentage - words_not_in_alignment_percentage)
+
+    total_score = score_words - score_groups
+##########################PLEASE REDO :D ###################################
 
     if should_print:
         print("goldstandardWordCount: " + str(goldstandardWordCount))
         print("alignedWordCount: " + str(alignedWordCount))
-        print("missingWords: " + str(missingWords))
         print("foundWords: " + str(foundWords))
+        print("words_in_gs_not_in_alignment_counter: " + str(words_in_gs_not_in_alignment_counter))
+        print("words_in_alignment_not_in_gs_counter: " + str(words_in_alignment_not_in_gs_counter))
+        print("words_not_in_alignment_percentage: " + str(words_not_in_alignment_percentage))
+        print("words_not_in_gs_percentage: " + str(words_not_in_gs_percentage))
         print("numberOfAlignedGroupes: " + str(numberOfAlignedGroupes))
         print("numberOfGSGroupes: " + str(numberOfGSGroupes))
         print("difference(abs(numberOfGSGroupes-numberOfAlignedGroupes)): " + str(
             abs(numberOfGSGroupes - numberOfAlignedGroupes)))
-        print("scoreWords: " + str(scoreWords))
-        print("scoreGroups: " + str(scoreGroups))
-        print("totalScore(higher is better): " + str(totalScore))
 
-    print(str(additional_Text) + " scored: " + str(totalScore))
-    return totalScore
+        print("word_count_score: " + str(word_count_score))
+        print("score_words: " + str(score_words))
+        print("score_groups: " + str(score_groups))
+        print("total_score(higher is better): " + str(total_score))
+
+    print(str(additional_Text) + " scored: " + str(total_score))
+    return total_score
 
 
-def iterative_testing(total_iterations, current_iteration, current_params, gs_graph):
-    print("####Iteration[" + str(current_iteration) + "]####")
-    graph = ()
-    iterationCount = 1
-    for taskID in options.GOLD_STANDARD_SET:
-        util.print_progress(iterationCount, len(options.GOLD_STANDARD_SET), prefix='Progress:', suffix='Complete')
-        group = allTaskByID[taskID][0]
-        graph = alignGraph.align_every_sentence_to_the_others(group, current_params[0], current_params[1],
-                                                              current_params[2])
-
-        iterationCount += 1
-    score = calculate_alignment_score(gs_graph, graph, "With params[" + str(current_params) + "]", True)
-
-    if current_iteration >= total_iterations:
-        return
-    else:
-        current_params = [current_params[0], current_params[1], round(current_params[2] - 0.05, 2)]
-        iterative_testing(total_iterations, current_iteration + 1, current_params, gs_graph)
+# def iterative_testing(total_iterations, current_iteration, current_params, gs_graph):
+#     print("####Iteration[" + str(current_iteration) + "]####")
+#     graph = ()
+#     iterationCount = 1
+#     for taskID in options.GOLD_STANDARD_SET:
+#         util.print_progress(iterationCount, len(options.GOLD_STANDARD_SET), prefix='Progress:', suffix='Complete')
+#         group = allTaskByID[taskID][0]
+#         graph = alignGraph.align_every_sentence_to_the_others(group, current_params[0], current_params[1],
+#                                                               current_params[2])
+#
+#         iterationCount += 1
+#     score = calculate_alignment_score(gs_graph, graph, "With params[" + str(current_params) + "]", True)
+#
+#     if current_iteration >= total_iterations:
+#         return
+#     else:
+#         current_params = [current_params[0], current_params[1], round(current_params[2] - 0.05, 2)]
+#         iterative_testing(total_iterations, current_iteration + 1, current_params, gs_graph)
 
 
 allTaskByID = util.loadDataFromCSVFile('../data/transcribe-2017-07-08.CSV')
@@ -87,8 +114,27 @@ allTaskByID = util.loadDataFromCSVFile('../data/transcribe-2017-07-08.CSV')
 gs_graph = nx.json_graph.node_link_graph(
     util.load_json("GoldStandard/gs_graph.json"))  # LOAD GOLDSTANDARD_GRAPH FROM JSON
 
-params = [nx.Graph(), alignGraph.ALIGNER_BLEUALIGN, 0.9]
-iterative_testing(20, 1, params, gs_graph)
+# alignedGraph = nx.json_graph.node_link_graph(
+#     util.load_json("dumpedGraph.json"))  # LOAD GOLDSTANDARD_GRAPH FROM JSON
+params = [nx.Graph(), alignGraph.ALIGNER_HUNALIGN, 0.49]
+
+alignedGraph = ()
+iterationCount = 1
+for taskID in options.GOLD_STANDARD_SET:
+    util.print_progress(iterationCount, len(options.GOLD_STANDARD_SET), prefix='Progress:', suffix='Complete')
+    group = allTaskByID[taskID][0]
+    alignedGraph = alignGraph.align_every_sentence_to_the_others(group, params[0], params[1], params[2])
+    iterationCount += 1
+# util.dump_dict_to_json(nx.node_link_data(alignedGraph), "dumpedGraph.json")
+
+score = calculate_alignment_score(gs_graph, alignedGraph, "With params[" + str(params) + "]", True)
+
+
+
+
+
+
+# iterative_testing(20, 1, params, gs_graph)
 
 # group = allTaskByID[2048][0]
 # graph = alignGraph.align_every_sentence_to_the_others(group, params[0], params[1], params[2])
